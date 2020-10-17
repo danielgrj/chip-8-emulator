a1a161cb3657092cdef2bc39821b7acca3ff0cc9
Daniel Garcia <jdanielink@gmail.com>
2020-10-17 17:50:20 -0500

    Init commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const element = document.createElement('div');
+element.innerText = 'Bundle works!';
+
+document.querySelector('#root').appendChild(element);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[contenthash].bundle.js',
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.ts', '.json'],
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './public/index.html'),
+    }),
+  ],
+};
